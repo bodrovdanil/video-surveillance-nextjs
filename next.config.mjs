5a6c3126a5f030aee7b042f82106307657bd21aa
bodrovdanil <danil.b.s@mail.ru>
2024-11-18 21:37:21 +0500

    Adding a docker image. Auto-deploy when push to the main branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/video-surveillance-nextjs',
+    assetPrefix: isProd ? '/video-surveillance-nextjs/' : '',
+};
 
 export default nextConfig;
+
