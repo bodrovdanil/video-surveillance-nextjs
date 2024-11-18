51136ce6d6250790f8d942d4e533f64140e8247d
bodrovdanil <danil.b.s@mail.ru>
2024-11-18 22:41:15 +0500

    update 2

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     basePath: '/video-surveillance-nextjs',
-    assetPrefix: isProd ? '/video-surveillance-nextjs/' : '',
+    assetPrefix: isProd ? '/video-surveillance-nextjs/' : '', 
 };
 
 export default nextConfig;
